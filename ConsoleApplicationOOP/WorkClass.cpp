6f97cad8d097b05b3b638dc46c4e2220d2ad18b1
max <maxrexfax@gmail.com>
2022-01-25 18:19:51 +0200

    empty

--- ConsoleApplicationOOP/WorkClass.cpp
+++ ConsoleApplicationOOP/WorkClass.cpp
@@ -6,18 +6,5 @@ void WorkClass::startWork() {
     static_cast<GoodBasic*>(universalPointer01)->toString();
     static_cast<Chipsi*>(universalPointer01)->toString();
 
-   // Chipsi chips01;
-    //chips01.toString();
-	//Freezer freezer01(1, 234, "Freezer Samsung", "Good thing...", 2, 34);
-   // freezer01.getCameras();
-	//Freezer* pointerFreezer01 = &freezer01;
-   // pointerFreezer01->getCameras();
-
-    //GoodBasic* universalPointer01 = new GoodBasic;
-   // GoodBasic* universalPointer02 = new Freezer(1, 234, "Freezer Samsung", "Good thing...", 2, 34);
-   // GoodBasic* universalPointer03 = new Software(1, 234, "Soft FOR Samsung", "Very Good thing...", 2, 34);
-
-    //static_cast<GoodBasic*>(universalPointer01)->toString();
-    //static_cast<GoodBasic*>(universalPointer02)->toString();
-   // static_cast<GoodBasic*>(universalPointer03)->toString();
+  //new data for pull request
 }
