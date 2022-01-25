285e7c6b334b38d68fd201ba4d733c5eef141de3
max <maxrexfax@gmail.com>
2022-01-25 18:24:35 +0200

    some changes

--- ConsoleApplicationOOP/WorkClass.cpp
+++ ConsoleApplicationOOP/WorkClass.cpp
@@ -6,5 +6,18 @@ void WorkClass::startWork() {
     static_cast<GoodBasic*>(universalPointer01)->toString();
     static_cast<Chipsi*>(universalPointer01)->toString();
 
+<<<<<<< Updated upstream
   //new data for pull request
+=======
+
+    int aaaa01 = 100;
+    string textToMove = "move to freezer branch";
+
+   /*Родительский класс GoodBasic включает 4 поля, обязательные для всех предметов
+       Родительский класс Электроприборы мог бы включать обязат. для всех ЭЛЕКТРОприборов
+                поля Энергопотребление, шумноть, класс энергоэкономии и другие
+       Конечный дочерний класс Холодильник получает от двух родителей поля и методы, обязательные для
+       предметов и электроприборов и реализует свои личные, которых не может быть у других классов
+       предметов.*/
+>>>>>>> Stashed changes
 }
